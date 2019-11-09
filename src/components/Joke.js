d02664c824f2a86fc63164382697fd882761a840
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-09 11:35:41 -0500

    refactors to redux hooks

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import {connect} from 'react-redux';
+import {connect, useSelector, useDispatch} from 'react-redux';
 import { fetchJoke } from '../actions/index';
 
 
 import { Button } from 'reactstrap';
 
 function Joke(props) {
+  const dispatch = useDispatch()
+  const joke = useSelector(state => state.joke)
+  const isFetching = useSelector(state => state.isFetching)
+  const error = useSelector(state => state.error)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    dispatch(fetchJoke())
+  }
 
   return (
     <div className='joke-container'>
-      <h5>{props.joke.setup}</h5>
-      <p>{props.joke.punchline}</p>
+      <h5>{joke.setup}</h5>
+      <p>{joke.punchline}</p>
 
-      <Button color="success" onClick={props.fetchJoke}>Get Joke</Button>
+      <Button color="success" onClick={handleSubmit}>Get Joke</Button>
     </div>
   )
 };
 
-const mapStateToProps = state => {
-  return {
-    joke: state.joke,
-    isFetching: state.isFetching,
-    error: state.error
-  }
-}
+export default Joke;
 
-export default connect(mapStateToProps, { fetchJoke} )(Joke)
+
+// const mapStateToProps = state => {
+//   return {
+//     joke: state.joke,
+//     isFetching: state.isFetching,
+//     error: state.error
+//   }
+// }
+
+// export default connect(mapStateToProps, { fetchJoke} )(Joke)
+
