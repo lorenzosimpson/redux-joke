ab0059611e95deb016af689a9cbf9fd00339aa09
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-02-13 21:10:39 -0500

    successfully refactors to class components, no redux

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,32 +1,43 @@
 import React from 'react';
-import {connect, useSelector, useDispatch} from 'react-redux';
-import { fetchJoke } from '../actions/index';
+import axios from 'axios';
 
 
 import { Button } from 'reactstrap';
 
-function Joke(props) {
-  const dispatch = useDispatch()
-  const joke = useSelector(state => state.joke)
-  const isFetching = useSelector(state => state.isFetching)
-  const error = useSelector(state => state.error)
-
-
-
-
-  const handleSubmit = e => {
-    e.preventDefault()
-    dispatch(fetchJoke())
+class Joke extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      joke: {}
+    }
   }
 
-  return (
-    <div className='joke-container'>
-      <h5>{joke.setup}</h5>
-      <p>{joke.punchline}</p>
 
-      <Button color="success" onClick={handleSubmit}>Get Joke</Button>
-    </div>
-  )
+  handleSubmit = e => {
+    e.preventDefault()
+    axios
+    .get(`https://official-joke-api.appspot.com/random_joke`)
+    .then(res => {
+      this.setState({
+        joke: res.data
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  render() {
+    return (
+      <div className='joke-container'>
+        <h5>{this.state.joke.setup}</h5>
+        <p>{this.state.joke.punchline}</p>
+  
+        <Button color="success" onClick={this.handleSubmit}>Get Joke</Button>
+      </div>
+    )
+  }
+  
 };
 
 export default Joke;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//redux
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
 
 import Joke from './components/Joke'
 import "./styles.css";
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
 
 function App() {
 
@@ -24,7 +18,5 @@ function App() {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-<Provider store={store}>
   <App />
-</Provider>
 , rootElement);
